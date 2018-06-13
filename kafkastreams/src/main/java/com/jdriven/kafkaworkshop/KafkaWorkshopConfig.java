00d3940816bd68c17531be177717c86972d50d21
Alfredo Ferreira <alfredo@weareadaptive.com>
2018-06-13 09:29:03 +0200

    Setting up Spring Kafka

--- kafkastreams/src/main/java/com/jdriven/kafkaworkshop/KafkaWorkshopConfig.java
+++ kafkastreams/src/main/java/com/jdriven/kafkaworkshop/KafkaWorkshopConfig.java
@@ -1,8 +1,10 @@
 package com.jdriven.kafkaworkshop;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @Configuration
+@EnableKafka
 public class KafkaWorkshopConfig {
 
 }
